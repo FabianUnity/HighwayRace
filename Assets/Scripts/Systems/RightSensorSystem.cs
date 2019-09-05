e4fb24a92612778eee7e5e82f8fb2a2afdf5f7e2
Fabian Castillo <spider@Fabians-MacBook-Pro.local>
2019-09-05 17:23:44 -0500

    WIP: Add right sensor system

---
+++ Assets/Scripts/Systems/RightSensorSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+public class RightSensorSystem : JobComponentSystem
+{
+    private BeginInitializationEntityCommandBufferSystem entityCommandBufferSystem;
+    
+    protected override void OnCreate()
+    {
+        entityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+    
+    [ExcludeComponent(typeof(LaneChangeComponent))]
+    struct RightSensorJob : IJobForEachWithEntity<PositionComponent, LaneComponent>
+    {
+        public EntityCommandBuffer.Concurrent CommandBuffer;
+        
+        public void Execute(Entity entity, int index, [ReadOnly]ref PositionComponent positionComponent, ref LaneComponent laneComponent)
+        {
+                
+        }
+    }
+    
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new RightSensorJob
+        {
+            CommandBuffer = entityCommandBufferSystem.CreateCommandBuffer().ToConcurrent()
+        }.Schedule(this, inputDeps);
+        
+        entityCommandBufferSystem.AddJobHandleForProducer(job);
+
+        return job;
+    }
+}
