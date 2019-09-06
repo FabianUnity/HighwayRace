8c4dc10a85c23c3993aaeba98ffb40c1a8c02ee2
Cristian Mazo <cristianm@unity3d.com>
2019-09-06 17:16:17 -0500

    added should overtake method

--- Assets/Scripts/Components/OvertakerComponent.cs
+++ Assets/Scripts/Components/OvertakerComponent.cs
@@ -14,4 +14,11 @@ public struct OvertakerComponent : IComponentData
     public float OvertakeEargerness;
     public float CarInLeftSpeed;
     public bool Blocked;
+
+    public static bool WantsToOvertake(float speed, float carInFrontSpeed, float overtakeDistance, float distance, float eagerness)
+    {
+        if (distance > overtakeDistance) return false;
+        var s = carInFrontSpeed / speed;
+        return s < eagerness;
+    }
 }
